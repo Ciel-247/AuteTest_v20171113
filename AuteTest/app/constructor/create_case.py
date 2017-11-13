ff1883dcfc06b93f76a3613565b5fb6e6da51cf5
lesq <ciel_247@hotmail.com>
2017-11-13 10:46:48 +0800

    111

--- AuteTest/app/constructor/create_case.py
+++ AuteTest/app/constructor/create_case.py
@@ -48,7 +48,7 @@ class CreateCase:
                 uri=rx[4].value,
                 body=rx[5].value,
                 kwassert=rx[6].value,
-                ifex
+                ifexecute=rx[7].value
             )
             query_info = UseCaseOperation.query_test_case_info(##[lesq??]这里的目的是想要供后续判断这条用例是否已经存在？但是query_test_case_info方法还没有完善？
                 uri=params_dict.get('uri'),
